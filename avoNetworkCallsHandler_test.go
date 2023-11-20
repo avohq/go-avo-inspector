3901cd20480ee9d1d188ea0d802ed9e0cf9d4770
Kevin Piotrkowski <kevin.piotrkowski@toggl.com>
2023-11-20 12:43:11 -0300

    Add test coverage for callInspectorWithBatchBody

--- avoNetworkCallsHandler_test.go
+++ avoNetworkCallsHandler_test.go
@@ -1,9 +1,97 @@
 package avoinspector
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"reflect"
+	"strings"
 	"testing"
 )
+
+func TestAvoNetworkCallsHandler_callInspectorWithBatchBody(t *testing.T) {
+	type testCase struct {
+		serverCallback func() *httptest.Server
+		event          any
+		expectedError  string
+	}
+
+	testCases := map[string]testCase{
+		"should log": {
+			serverCallback: func() *httptest.Server {
+				return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(http.StatusOK)
+					w.Write([]byte(`{"samplingRate": 0.001}`))
+				}))
+			},
+			event: map[string]any{
+				"number_prop":  1,
+				"string_prop":  "test",
+				"boolean_prop": true,
+			},
+		},
+		"detect unmarshable event": {
+			serverCallback: func() *httptest.Server {
+				return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+			},
+			event:         func() {},
+			expectedError: "could not marshal events",
+		},
+		"detect invalid response status code": {
+			serverCallback: func() *httptest.Server {
+				return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(http.StatusBadRequest)
+				}))
+			},
+			event: map[string]any{
+				"number_prop":  1,
+				"string_prop":  "test",
+				"boolean_prop": true,
+			},
+			expectedError: "request returned non-200 status code",
+		},
+		"detect invalid response body": {
+			serverCallback: func() *httptest.Server {
+				return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(http.StatusOK)
+				}))
+			},
+			event: map[string]any{
+				"number_prop":  1,
+				"string_prop":  "test",
+				"boolean_prop": true,
+			},
+			expectedError: "failed to parse response body",
+		},
+	}
+
+	for testName, tc := range testCases {
+		t.Run(testName, func(t *testing.T) {
+			// Setup a test server to mock the communication with Avo Inspector
+			srv := tc.serverCallback()
+			defer srv.Close()
+
+			// Create an instance of AvoNetworkCallsHandler
+			handler := &AvoNetworkCallsHandler{
+				apiKey:           "test-api-key",
+				envName:          "test",
+				appName:          "test-app",
+				appVersion:       "1.0.0",
+				libVersion:       "1.0.0",
+				samplingRate:     1.0,
+				shouldLog:        false,
+				trackingEndpoint: srv.URL,
+			}
+
+			// Verify the result
+			err := handler.callInspectorWithBatchBody([]any{tc.event})
+			if err == nil && tc.expectedError != "" {
+				t.Errorf("expected error '%s' but got none", tc.expectedError)
+			} else if err != nil && !strings.Contains(err.Error(), tc.expectedError) {
+				t.Errorf("unexpected error, got: %s and expected '%s'", err, tc.expectedError)
+			}
+		})
+	}
+}
 
 func TestAvoNetworkCallsHandler_bodyForSessionStartedCall(t *testing.T) {
 	// Create an instance of AvoNetworkCallsHandler
